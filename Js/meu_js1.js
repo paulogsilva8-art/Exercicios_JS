f21748c23e7eecf5048e92d5a9a32ad054bc6a9b
paulogsilva8-art <paulo.g.silva8@edu.senai.br>
2026-06-15 15:51:26 -0300

    1506

---
+++ Js/meu_js1.js
@@ -0,0 +1,7 @@
+//string
+        var texto = 'Curso de JavaScript';
+        //number
+        var numberInteiro = -7;
+        var numberFracionado = 10.45;
+        //boolean
+        var teste = true; //true-false
